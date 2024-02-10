4fb66d3f278802b97d156e3ce3219517de4e6b1e
Mayur <mayurpmali7@gmail.com>
2024-02-10 21:53:41 +0530

    added some folders to share and manage code

--- react/home_appliances/src/App.js
+++ react/home_appliances/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AppForm from './components/owner/appliance_registration';
+
 
 function App() {
   return (
-    <div className="App">
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +21,9 @@ function App() {
         </a>
       </header>
     </div>
+    */
+    <AppForm/>
+    
   );
 }
 
