6595380838ac553274fb27a2df0f249b92ddb106
69187kunalm <128111639+69187kunalm@users.noreply.github.com>
2024-02-22 00:11:41 +0530

    Transaction And User Controllers added
    .net

---
+++ kunal/DotNetBackend/Controllers/TransactionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using RentalApp.Models;
+
+namespace RentalApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [EnableCors]
+    public class TransactionController : Controller
+    {
+        [HttpGet]
+        public List<Transaction>  GetTransactionDetails() 
+        {
+            List<Transaction> tlist = new List<Transaction>();
+            using (var db = new rentalappContext())
+            {
+                tlist = db.Transactions.ToList();
+            }
+            return tlist;
+        }
+
+
+      
+    }
+}
---
+++ kunal/DotNetBackend/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using RentalApp.Models;
+using System.Linq;
+
+namespace RentalApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [EnableCors]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetUserDetails([FromQuery] int id)
+        {
+            using (var db = new rentalappContext())
+            {
+                var user = db.Users
+                    .Where(u => u.UserId == id)
+                    .FirstOrDefault();
+
+                if (user == null)
+                {
+                    return NotFound(); 
+                }
+
+                return Ok(user);
+            }
+        }
+        [HttpGet]
+        public List<User> GetUserAllDetails()
+        {
+            List<User> tlist = new List<User>();
+            using (var db = new rentalappContext())
+            {
+                tlist = db.Users.ToList();
+            }
+            return tlist;
+        }
+    }
+}
